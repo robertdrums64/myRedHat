0798a130437f477c317291c5cd6ae1f714f6595f
Robert De La Torre <robertdrums64@gmail.com>
2025-05-03 20:32:02 -0700

    Updated folders

--- main.tf
+++ main.tf
@@ -6,10 +6,3 @@
          }
        }
      }
-
-     provider "aws" {
-       region = "us-east-1"
-       # You can also use environment variables for AWS credentials
-       # aws_access_key_id = "YOUR_ACCESS_KEY"
-       # aws_secret_access_key = "YOUR_SECRET_KEY"
-     }
