b6770ad6c1f22d31f828d0f4c17a2cd32e53f4f1
Robert De La Torre <robertdrums64@gmail.com>
2025-05-03 21:01:37 -0700

    comments added

--- providers.tf
+++ providers.tf
@@ -1,6 +1,6 @@
      provider "aws" {
        region = "us-east-1"
        # You can also use environment variables for AWS credentials
-       aws_access_key_id = "AWS_ACCESS_KEY_ID"
-       aws_secret_access_key = "AWS_SECRET_ACCESS_KEY"
+       # aws_access_key_id = "AWS_ACCESS_KEY_ID"
+       # aws_secret_access_key = "AWS_SECRET_ACCESS_KEY"
      }
