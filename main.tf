f8eb7b2d54c6a1e36dbe79294fafacc3dd77c936
Robert De La Torre <robertdrums64@gmail.com>
2025-05-03 22:51:52 -0700

    fixed resource

--- main.tf
+++ main.tf
@@ -11,33 +11,7 @@
     region = "us-east-1"
   }
 
-  resource "aws_instance" "web" {
+  resource "aws_instance" "myRHEL" {
     ami                                  = "ami-0c15e602d3d6c6c4a"
-    associate_public_ip_address          = true
-    availability_zone                    = "us-east-1c"
-    cpu_core_count                       = 1
-    cpu_threads_per_core                 = 1
-    disable_api_termination              = false
-    ebs_optimized                        = false
-    get_password_data                    = false
-    hibernation                          = false
-    host_id                              = null
-    iam_instance_profile                 = null
-    instance_initiated_shutdown_behavior = "stop"
     instance_type                        = "t2.micro"
-    ipv6_address_count                   = 0
-    ipv6_addresses                       = []
-    key_name                             = "myKeyPair"
-    monitoring                           = false
-    placement_group                      = null
-    placement_partition_number           = null
-    private_ip                           = "172.31.87.107"
-    secondary_private_ips                = []
-    security_groups                      = ["launch-wizard-1"]
-    source_dest_check                    = true
-    subnet_id                            = "subnet-061981f57679fbe5e"
-    
-    tags = {
-      Name = "myRHEL"
-    }
   }
