ec28364bc13a8b0c130d8d519c7deaa00b764607
Robert De La Torre <robertdrums64@gmail.com>
2025-05-03 19:00:00 -0700

    Added enironment folders

--- main.tf
+++ main.tf
@@ -1,1 +1,15 @@
-##Empty main
+     terraform {
+       required_providers {
+         aws = {
+           source  = "hashicorp/aws"
+           version = "~> 3.0"
+         }
+       }
+     }
+
+     provider "aws" {
+       region = "us-east-1"
+       # You can also use environment variables for AWS credentials
+       # aws_access_key_id = "YOUR_ACCESS_KEY"
+       # aws_secret_access_key = "YOUR_SECRET_KEY"
+     }
